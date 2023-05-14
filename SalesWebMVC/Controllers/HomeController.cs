ec108ea9ea486cff881eeb91dcefd0ef29407ad4
Geraldo Nteka <geraldonteka10@gmail.com>
2023-05-14 14:04:04 +0100

    First controller, Razor pages tests and add page About and Contact

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -18,6 +18,24 @@ namespace SalesWebMVC.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web MVC Application";
+            ViewData["Author"] = "Epeneto Geraldo Nteka";
+            ViewData["Email"] = "geraldonteka10@gmail.com";
+            ViewData["LinkedIn"] = "https://www.linkedin.com/in/geraldonteka/";
+            ViewData["GitHub"] = "https://github.com/geraldonteka";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
