4af56343f3531beff5e3c5fda1410734c94c3142
Geraldo Nteka <geraldonteka10@gmail.com>
2023-05-15 23:23:30 +0100

    First MVC - Department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departmentsList = new List<Department>();
+            departmentsList.Add(new Department { Id = 1, Name = "Eletronics" });
+            departmentsList.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(departmentsList);
+        }
+    }
+}
