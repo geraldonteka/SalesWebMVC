da89ada506a5f926fce0429f72d3b25c88829a8d
Geraldo Nteka <geraldonteka10@gmail.com>
2023-05-20 15:57:08 +0100

    Other entities and second migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -9,11 +9,13 @@ namespace SalesWebMVC.Data
 {
     public class SalesWebMVCContext : DbContext
     {
-        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
+        public SalesWebMVCContext(DbContextOptions<SalesWebMVCContext> options)
             : base(options)
         {
         }
 
         public DbSet<SalesWebMVC.Models.Department> Department { get; set; } = default!;
+        public DbSet<SalesWebMVC.Models.Seller> Seller { get; set; } = default!;
+        public DbSet<SalesWebMVC.Models.SalesRecord> SalesRecord { get; set; } = default!;
     }
 }
