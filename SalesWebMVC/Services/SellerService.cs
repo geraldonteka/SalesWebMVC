597bd929a1a84ca237ef7f5fa0a639accc3c77da
Geraldo Nteka <geraldonteka10@gmail.com>
2023-05-25 17:55:02 +0100

    Simple Create form | Foreign key not null (referential integrity)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
@@ -15,6 +16,13 @@ namespace SalesWebMVC.Services
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            obj.Department = _context.Department.First();
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
