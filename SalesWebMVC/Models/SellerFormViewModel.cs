a7c62a0d23a196ddcb4ef343e30a7c9384f3bb9c
Geraldo Nteka <geraldonteka10@gmail.com>
2023-05-26 17:02:29 +0100

    SellerFormViewModel and Department select component

---
+++ SalesWebMVC/Models/SellerFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options =>
     options.UseMySql(connectionString, serverVersion));
 
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
