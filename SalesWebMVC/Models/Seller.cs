6dd615f690141640c1217f3c859f75b54cf4c5d0
Geraldo Nteka <geraldonteka10@gmail.com>
2023-06-05 13:02:06 +0100

    Update views

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -29,6 +29,8 @@ namespace SalesWebMVC.Models
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
+
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
 
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
